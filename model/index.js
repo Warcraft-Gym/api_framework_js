6313cf9126d93d409c45b0f3758a26aeada30d21
Martin Bauer <eashibby@gmx.de>
2025-05-14 12:00:23 +0200

    index.js

---
+++ model/index.js
@@ -0,0 +1,11 @@
+import Match from './match.js';
+import Season from './season.js';
+import Series from './series.js';
+import Team from './team.js';
+import User from './user.js';
+import Map from './map.js';
+import FantasyTeam from './fantasy_team.js';
+import FantasyBet from './fantasy_bet.js';
+import SeasonInfo from './season_info.js'
+
+export { Match, Season, SeasonInfo, Series, Team, User, Map, FantasyTeam, FantasyBet };
---
+++ services/index.js
@@ -0,0 +1,9 @@
+import UserService from './user_service.js';
+import MatchService from './match_service.js';
+import TeamService from './team_service.js';
+import SeriesService from './series_service.js';
+import SeasonService from './season_service.js';
+import FantasyBetService from './fantasy_bet_service.js';
+import FantasyTeamService from './fantasy_team_service.js';
+
+export { UserService, MatchService, TeamService, SeriesService, SeasonService, FantasyBetService, FantasyTeamService };
