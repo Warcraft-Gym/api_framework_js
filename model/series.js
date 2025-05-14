af8d3bd7ce7009e0b015792945a1ea843025464f
Martin Bauer <eashibby@gmx.de>
2025-05-14 23:03:31 +0200

    fix series

--- model/series.js
+++ model/series.js
@@ -13,10 +13,10 @@ class Series {
         this.player1 = data.player1 ? new User(data.player1) : null;
         this.player2_id = data.player2_id || null;
         this.player2 = data.player2 ? new User(data.player2) : null;
-        this.player1Score = data.player1_score || null;
+        this.player1_score = data.player1_score || null;
         this.player2_score = data.player2_score || null;
         this.player1_points = data.player1_points || null;
-        this.player2Points = data.player2_points || null;
+        this.player2_points = data.player2_points || null;
         this.host_player_id = data.host_player_id || null;
         this.is_fantasy_match = data.is_fantasy_match || false;
     }
@@ -28,10 +28,10 @@ class Series {
             caster: this.caster,
             player1_id: this.player1_id,
             player2_id: this.player2_id,
-            player1Score: this.player1Score,
+            player1_score: this.player1_score,
             player2_score: this.player2_score,
             player1_points: this.player1_points,
-            player2Points: this.player2Points,
+            player2_points: this.player2_points,
             host_player_id: this.host_player_id,
             is_fantasy_match: this.is_fantasy_match
         };
@@ -42,8 +42,8 @@ class Series {
                `date_time=${this.date_time}, caster=${this.caster}, ` +
                `player1_id=${this.player1_id}, player1=${this.player1}, ` +
                `player2_id=${this.player2_id}, player2=${this.player2}, ` +
-               `player1Score=${this.player1Score}, player2_score=${this.player2_score}, ` +
-               `player1_points=${this.player1_points}, player2Points=${this.player2Points}, ` +
+               `player1_score=${this.player1_score}, player2_score=${this.player2_score}, ` +
+               `player1_points=${this.player1_points}, player2_points=${this.player2_points}, ` +
                `host_player_id=${this.host_player_id}, is_fantasy_match=${this.is_fantasy_match})`;
     }
 }
